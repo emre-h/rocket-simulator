ee3b592176a7aa1d3480615698b5dc1813ebaa5c
Emre Harbutoğlu <emre.harbutoglu@hemenis.com>
2020-06-19 13:11:32 +0300

    added avionic systems

--- test.py
+++ test.py
@@ -32,6 +32,8 @@ def readThrustValues(filePath):
 readThrustValues(thrustFilePath)
 
 stage = 1
+cd = 0.421
+area = math.pi*((0.06)*(0.06))
 
 def averageThrust():
     r = 0
@@ -41,7 +43,9 @@ def averageThrust():
     return r/len(engineThrust)
 
 def runPhysics():
-    payloadWeight = 4
+    global dt, cd, tBefore, oldAltitude, area, stage
+
+    payloadWeight = 4.5
     thrust = 0.0
     tBefore = 0.0
     velocity = 0.6
@@ -51,9 +55,7 @@ def runPhysics():
     angle = 85
     acceleration = 0.0
     airResistance = 0.0
-    noiseArea = math.pi*((0.06)*(0.06))
     temperature = 15
-    cd = 0.42
     currentMass = 0.0
     terminalVelocity = 0.0
     landingAltitude = 0.0
@@ -84,11 +86,15 @@ def runPhysics():
             #hpa -> pa
             density = 100*pressure / ((temperature + 273.15)*(287.05))
 
-            base = (1-((0.0065*altitude)/(temperature + 273.15 + 0.0065*altitude)))
+            calc = (1-((0.0065*altitude)/(temperature + 273.15 + 0.0065*altitude)))
 
-            pressure = 1013.25*math.pow(base, 5.257)
+            try:
+                pressure = 1013.25*math.pow(calc, 5.257)
+            except:
+                print(calc)
+                break
 
-            airResistance = cd*((velocity*velocity)*density*noiseArea)/2
+            airResistance = cd*((velocity*velocity)*density*area)/2
 
             acceleration = ((thrust - airResistance - g*currentMass) / currentMass)*math.sin(math.pi*angle/180)
 
@@ -113,26 +119,24 @@ def runPhysics():
                 velocity = 0
                 apogeeTime = t
                 altitude = 1
-                cd = 0.5
-                noiseArea = (1.2)*(1.2)*math.pi
                 currentMass = rocketWeight - payloadWeight
                 airResistance = 0
-                terminalVelocity = math.sqrt((8*currentMass*g)/(density*cd*noiseArea))
                 status = "landing"
+                terminalVelocity = math.sqrt((8*currentMass*g)/(density*cd*area))
                 print("Terminal velocity: " + str(terminalVelocity) + "\n")
         else:
-            if altitude % 200 == 0:
+            if landingAltitude % 200 == 0:
                 temperature += 1
 
             density = 100*pressure / ((temperature + 273.15)*(287.05))
 
-            base = (1-((0.0065*altitude)/(temperature + 273.15 + 0.0065*altitude)))
+            base = (1-((0.0065*landingAltitude)/(temperature + 273.15 + 0.0065*landingAltitude)))
 
             pressure = 1013.25*math.pow(base, 5.257)
 
-            airResistance = cd*((velocity*velocity)*density*noiseArea)/2
+            airResistance = cd*((velocity*velocity)*density*area)/2
 
-            acceleration = ((g*currentMass - airResistance) / currentMass)*math.sin(math.pi*angle/180)
+            acceleration = ((-g*currentMass + airResistance) / currentMass)*math.sin(math.pi*angle/180)
 
             deltaT = t - tBefore
 
@@ -141,7 +145,7 @@ def runPhysics():
             if velocity >= terminalVelocity:
                 velocity = terminalVelocity
 
-            altitude += velocity * deltaT
+            altitude += -1*(velocity * deltaT)
 
             landingAltitude = apogee - altitude
 
@@ -155,34 +159,84 @@ def runPhysics():
                 print("Velocity: " + str(velocity))
                 print("Altitude: " + str(landingAltitude))
                 print("Acceleration: " + str(acceleration) + "\n")
+                stage = 4
                 status = "landed"
                 break
 
-        tBefore = t
+        primaryAvionicSystem(t, pressure, temperature, acceleration)
+        secondaryAvionicSystem(t, acceleration)
 
+        tBefore = t
     # print(velocity)
     # print(altitude)
     # print(landingAltitude)
-
     return None
 
-def recovery():
+primaryRecvoeryMode = False
+secondaryRecvoeryMode = False
+
+def firstRecovery():
+    global cd, status, stage
+    # parachute
+    area = (1.2)*(1.2)*math.pi
+    cd = 0.5
+    stage = 2
     status = "recovery"
-    print("Recovery mode")
+    print("Recovery mode\n")
     return 1
 
-def avionicAlgorithm(velocity, altitude, oldAltitude, acceleration):
-    print(datetime.datetime.now())
-    print("Status" + status)
-    print("Velocity" + velocity)
-    print("Altitude" + altitude)
-    print("Old altitude" + oldAltitude)
-    print("Acceleration" + acceleration)
+oldAltitude = 0
+dt = 0.001
 
-    if velocity < 0 or (oldAltitude - altitude) > 0:
-        recovery()
+def primaryAvionicSystem(t,pressure, temperature, accelerationY):
+    global dt, cd, oldAltitude,primaryRecvoeryMode ,stage
 
-    return None
+    #FINDING VELOCITY FROM ALTITUDE
+
+    altitude = ((math.pow(1013.25/pressure, 1/5.257)-1)*(temperature + 273.15)) / 0.0065
+
+    velocity = (altitude - oldAltitude) / dt
+
+    if not primaryRecvoeryMode and velocity >= -2 and velocity <= 0 and altitude < 3060 and altitude > 3000:
+        print("PRIMARY AVIONIC - RECOVERY MODE SIGNAL RECEIVED")
+        print("Time: " + str(t))
+        print("Pressure: " + str(pressure))
+        print("Temperature: " + str(temperature))
+        print("Velocity: " + str(velocity))
+        print("Altitude: " + str(altitude))
+        print("Acceleration: " + str(accelerationY) + "\n")
+        primaryRecvoeryMode = True
+        firstRecovery()
+        return 1
+
+    oldAltitude = altitude
+
+    return 0
+
+velocityVector = 0
+altitudeVector = 0
+
+def secondaryAvionicSystem(t, accelerationY):
+    global velocityVector, altitudeVector, secondaryRecvoeryMode, dt
+
+    #FINDING VELOCITY AND ALTITUDE FROM ACCELERATION
+
+    velocityVector += accelerationY*dt
+
+    altitudeVector += velocityVector*dt
+
+    if not secondaryRecvoeryMode and velocityVector >= -3 and velocityVector <= -1 and altitudeVector < 3060 and altitudeVector >= 3000:
+        print("SECONDARY AVIONIC - RECOVERY MODE SIGNAL RECEIVED")
+        print("Time: " + str(t))
+        print("Calculated altitude: " + str(altitudeVector))
+        print("Velocity: " + str(velocityVector))
+        print("Acceleration: " + str(accelerationY) + "\n")
+        secondaryRecvoeryMode = True
+        firstRecovery()
+        return 1
+
+    return 0
+
 
 def startSimulation():
 
